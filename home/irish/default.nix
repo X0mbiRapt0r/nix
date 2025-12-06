4c4c0073b8dbf69afeb28579ceefc09b205a27c8
X0mbiRapt0r <11319449+X0mbiRapt0r@users.noreply.github.com>
2025-12-06 17:36:24 +0200

    darwin flake tweaks

--- home/irish/default.nix
+++ home/irish/default.nix
@@ -5,20 +5,9 @@
   home.username = "irish";
   home.homeDirectory = "/Users/irish";
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    initContent = ''
-      export EDITOR=nvim
-      # Shorter prompt: user@host:~/path >
-      # %~ = nice, shortened path (uses ~ for $HOME and collapses middle)
-      PROMPT='%n@%m:%1~ > '
-      alias gs="git status"
-      alias ll="ls -lah"
-    '';
+  targets.darwin.defaults.NSGlobalDomain = {
+    AppleLanguages = [ "en-GB" ];
+    AppleLocale = "en_ZA";
   };
 
   programs.git = {
@@ -35,6 +24,22 @@
     enable = true;
     defaultEditor = true;
     vimAlias = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    initContent = ''
+      export EDITOR=nvim
+      # Shorter prompt: user@host:~/path >
+      # %~ = nice, shortened path (uses ~ for $HOME and collapses middle)
+      PROMPT='%n@%m:%1~ > '
+      alias gs="git status"
+      alias ll="ls -lah"
+    '';
   };
 
   home.packages = with pkgs; [
