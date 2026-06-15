62a040b3364f94b32e427cfb9cf2a33a73049ba6
X0mbiRapt0r <11319449+X0mbiRapt0r@users.noreply.github.com>
2026-06-15 14:27:21 +0200

    quick update

--- hosts/QTM-Irish-MBA/host_darwin.nix
+++ hosts/QTM-Irish-MBA/host_darwin.nix
@@ -1,11 +1,15 @@
 { ... }:
 
 {
-  homebrew.casks = [
-    "docker-desktop" # Docker Desktop for Mac.
-    "powershell" # Microsoft PowerShell app/runtime.
-    "windows-app" # Microsoft Windows App for remote desktops/cloud PCs.
-  ];
+  homebrew = {
+    casks = [
+      "docker-desktop" # Docker Desktop for Mac.
+      "windows-app" # Microsoft Windows App for remote desktops/cloud PCs.
+    ];
+    formulas = [
+      "powershell" # Microsoft Azure CLI tool.
+    ];
+  };
 
   networking = {
     computerName = "QTM-Irish-MBA"; # User-visible macOS computer name.
